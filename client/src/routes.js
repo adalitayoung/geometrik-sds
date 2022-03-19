0dc5f2712f49134f8432ae572f5c608ac414fb55
AbdullahS98 <abdullahslimani98@gmail.com>
2022-03-19 21:56:04 +1100

    updated package.json to include react-router-dom

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,21 +1,21 @@
-import React, { useReducer } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import App from './App';
+import React, { useReducer } from "react";
+import { Route, Switch } from "react-router-dom";
+import App from "./App";
 
 /**
  * Routes manages the system routing. Will return the first match so always keep the empty routes at the bottom
  */
 function Routes() {
-    return (
-        <Switch>
-            {/* <Route path="/register">
+  return (
+    <Switch>
+      {/* <Route path="/register">
                 <Registration/>
             </Route> */}
-            <Route path="/">
-                <App />
-            </Route>
-        </Switch>
-    ) 
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  );
 }
 
-export default Routes
+export default Routes;
