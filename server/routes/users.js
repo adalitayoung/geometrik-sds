d76eec4a3b693a6f07edbc09b09d8e73853861cf
helenatran <tranhelena99@gmail.com>
2022-03-21 16:33:50 +1100

    Validate user doesn't already exist by checking their email against the database

--- server/routes/users.js
+++ server/routes/users.js
@@ -19,27 +19,37 @@ router.get("/", function (req, res, next) {
 // POST a new user (i.e. Create/Register a user).
 router.post("/", function (req, res, next) {
   const { firstName, lastName, email, password, role, designProgram, position, yearsOfExperience } = req.body;
-  pgClient.query(
-    'INSERT INTO "User" ("firstName", "lastName", email, password, role, "designProgram", "position", "yearsOfExperience") ' +
-      "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-    [
-      firstName,
-      lastName,
-      email,
-      password,
-      role,
-      designProgram === "" ? null : designProgram,
-      position === "" ? null : position,
-      yearsOfExperience === "" ? null : yearsOfExperience,
-    ],
-    function (err, results) {
-      if (err) {
-        res.status(500).send(err.message);
-      } else {
-        res.status(201).send("User registered!");
-      }
+
+  // Verify that the email has not already been registered
+  pgClient.query('SELECT EXISTS (SELECT 1 FROM "User" WHERE email=$1)', [email], function (err, results) {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (results.rows[0].exists) {
+      res.status(400).send("User email already exists");
+    } else {
+      pgClient.query(
+        'INSERT INTO "User" ("firstName", "lastName", email, password, role, "designProgram", "position", "yearsOfExperience") ' +
+          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+        [
+          firstName,
+          lastName,
+          email,
+          password,
+          role,
+          designProgram === "" ? null : designProgram,
+          position === "" ? null : position,
+          yearsOfExperience === "" ? null : yearsOfExperience,
+        ],
+        function (err, results) {
+          if (err) {
+            res.status(500).send(err.message);
+          } else {
+            res.status(201).send("User registered!");
+          }
+        }
+      );
     }
-  );
+  });
 });
 
 module.exports = router;
