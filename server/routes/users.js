1bb09e69df3aa6626271e7cdc9280fca2012b89e
helenatran <tranhelena99@gmail.com>
2022-03-25 17:12:12 +1100

    Update registration function for Supplier and Designer tables

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,9 @@ const db = require("../db");
 var router = express.Router();
 const pgClient = db.getDb();
 
+const SUPPLIER = "Supplier";
+const DESIGNER = "Designer";
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   pgClient.query('SELECT * FROM "User"', function (err, results) {
@@ -17,36 +20,44 @@ router.get("/", function (req, res, next) {
 
 // POST a new user (i.e. Create/Register a user).
 router.post("/", function (req, res, next) {
-  const { firstName, lastName, email, password, role, designProgram, position, yearsOfExperience } = req.body;
-
   // Verify that the email has not already been registered
-  pgClient.query('SELECT EXISTS (SELECT 1 FROM "User" WHERE email=$1)', [email], function (err, results) {
+  pgClient.query('SELECT EXISTS (SELECT 1 FROM "User" WHERE email=$1)', [req.body.email], function (err, results) {
     if (err) {
       res.status(500).send(err.message);
     } else if (results.rows[0].exists) {
       res.status(400).send("User email already exists");
     } else {
-      pgClient.query(
-        'INSERT INTO "User" ("firstName", "lastName", email, password, role, "designProgram", "position", "yearsOfExperience") ' +
-          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-        [
-          firstName,
-          lastName,
-          email,
-          password,
-          role,
-          designProgram === "" ? null : designProgram,
-          position === "" ? null : position,
-          yearsOfExperience === "" ? null : yearsOfExperience,
-        ],
-        function (err, results) {
-          if (err) {
-            res.status(500).send(err.message);
-          } else {
-            res.status(201).send("User registered!");
+      // Add user to Supplier or Designer table based on their role
+      const { name, email, password, role, is_active, discipline, position } = req.body;
+      if (role === SUPPLIER) {
+        pgClient.query(
+          'INSERT INTO "Supplier" (name, email, password, role, is_active, discipline, position, is_approved) ' +
+            "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+          [name, email, password, role, is_active, discipline, position, req.body.is_approved],
+          function (err, results) {
+            if (err) {
+              res.status(500).send(err.message);
+            } else {
+              res.status(201).send("Supplier registered!");
+            }
           }
-        }
-      );
+        );
+      } else if (role === DESIGNER) {
+        pgClient.query(
+          'INSERT INTO "Designer" (name, email, password, role, is_active, discipline, position, design_program, experience) ' +
+            "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+          [name, email, password, role, is_active, discipline, position, req.body.design_program, req.body.experience],
+          function (err, results) {
+            if (err) {
+              res.status(500).send(err.message);
+            } else {
+              res.status(201).send("Designer registered!");
+            }
+          }
+        );
+      } else {
+        res.status(400).send("The user role must be Supplier or Designer");
+      }
     }
   });
 });
